356a82707e7c0cfcaef84bf65972339f12df38f5
Tim Shields <tim.shields132@gmail.com>
2020-05-18 01:39:47 -0400

    sdf

---
+++ weather.py
@@ -0,0 +1,24 @@
+import requests, json, decimal
+from decimal import Decimal
+
+def tempconvert(arg):
+    y = (int(arg) - 273.15) * 9/5 + 32
+    return Decimal(y).quantize(Decimal("1"))
+
+zip = input("Enter zip code:")
+appid = "98d6b9ebdd773ed18ee226d7b3010185"
+url = ("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&appid=" + appid)
+response = requests.get(url)
+r = response.json()
+x = r["main"]
+
+TemperatureK = x["temp"]
+
+"""
+TemperatureF = (int(TemperatureK) - 273.15) * 9/5 + 32
+TemperatureFinal = Decimal(TemperatureF).quantize(Decimal("1"))
+"""
+
+print("Temperature = " + str(tempconvert(TemperatureK)) + " F")
+
+#print(response.content)
