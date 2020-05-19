c2e0beb7bc8ea059566a8e73fd27698e6d3333e1
Tim Shields <tim.shields132@gmail.com>
2020-05-19 00:43:19 -0400

    19 broken

--- weather.py
+++ weather.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 
 def tempconvert(arg):
     y = (int(arg) - 273.15) * 9/5 + 32
+    #Kelvin to Fahrenheit
     return Decimal(y).quantize(Decimal("1"))
 
 zip = input("Enter zip code:")
@@ -11,8 +12,9 @@ url = ("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&appid=
 response = requests.get(url)
 r = response.json()
 x = r["main"]
+z = r["weather"]
 
-TemperatureK = x["temp"]
-print("Temperature = " + str(tempconvert(TemperatureK)) + " F")
-
-#print(response.content)
+#TemperatureK = x["temp"]
+print("The temperature in " + str(r["name"]) + " is " + str(tempconvert(x["temp"])) + "\xb0 F. It feels like " + str(tempconvert(x["feels_like"])) + "\xb0 F.")
+print("Outside, it is currently " + int(z["description"]) + ".")
+print(response.content)
