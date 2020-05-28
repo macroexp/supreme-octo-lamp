eaf3f90175f4d2709bd18bff4cddd7450345f6bd
Tim Shields <tim.shields132@gmail.com>
2020-05-28 15:19:33 -0400

    fixed stuff + error management

--- weather.py
+++ weather.py
@@ -1,4 +1,4 @@
-import requests, json, decimal
+import requests, json, decimal, sys
 from decimal import Decimal
 
 def tempconvert(arg):
@@ -9,38 +9,32 @@ def tempconvert(arg):
 
 while True:
     zip = input("Enter zip code:")
-    if not len(zip) == 5 and zip.isdigit():
-    #check for 5 digit zip code
+    if not len(zip) == 5 and zip.isdigit(): #check for 5 digit zip code
         print("Sorry, that wasn't a valid zip code. Please try again.")
         continue
     else:
         break
 
-appid = "98d6b9ebdd773ed18ee226d7b3010185"
+appid = "98d6b9ebdd773ed18ee226d7b3010185" #actual api key
+#appid = "23" #crap testing api key for errors
 url = ("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&appid=" + appid)
 response = requests.get(url)
 r = response.json()
-x = r["main"]
-z = r["weather"]
-
-"""
-while True:
-    if response != 200:
-        print("Sorry, something went wrong.")
-        continue
-    else:
-        break
-"""
-"""
 try:
-    print(response) 
+    x = r["main"]
+    z = r["weather"]
 except KeyError:
-    print("pioops")
-"""
+    sys.exit("An error arose with the API. Please try again.")
 
 
-#TemperatureK = x["temp"]
+if response:
+    #response allows codes from 200 to 400
+    pass
+else:
+    sys.exit("Something went wrong with the API.")
+
 print("The temperature in " + str(r["name"]) + " is " + str(tempconvert(x["temp"])) + "\xb0 F. It feels like " + str(tempconvert(x["feels_like"])) + "\xb0 F.")
 print("Outside, it is currently " + z[0]["description"] + ".")
+
 #print(response.content)
-print(response.status_code)
+#print(response.status_code)
