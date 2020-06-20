d69f622437405b8fc2481a73f14eeb229ca0f941
Tim Shields <tim.shields132@gmail.com>
2020-06-20 17:32:31 -0400

    stuff based off jupyterlab

--- test.py
+++ test.py
@@ -1,15 +1,37 @@
-"""
-num = input("enter a 4 digit number:")
-if len(num) == 4 and num.isdigit():
-    print("yay!")
-elif len(num) != 4:
-    print("aw no!")
-"""
+class Food:
+    type = "unknown"
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+    def __str__(self):
+        #print self.name + " color: " + self.color 
+        return("Generic Food: %s" % self.name) 
+    def __repr__(self):
+        return self.__str__()
 
-zip = input("Enter zip code:")
-while len(zip) != 5:
-    #check for 5 digit zip code
-    pass
-else:
-    print(zip)
+class Green(Food):
+    type = "veggie"
+    def __str__(self):
+        #print("Green vegatable: %s " % self.name.capitalize())
+        return "Green vegatable: %s " % self.name.capitalize() + "; " + self.name.capitalize() + " color: %s " % self.color.capitalize()
 
+class RootVeg(Food):
+    type = "veggie"
+    def __str__(self):
+        #print("Root vegatable: %s " % self.name.capitalize())
+        return "Root vegatable: %s " % self.name.capitalize() + "; " + self.name.capitalize() + " color: %s " % self.color.capitalize()
+        
+class Poultry(Food):
+    type = "meat"
+    def __str__(self):
+        #print("Poultry: %s " % self.name.capitalize())
+        return "Poultry: %s " % self.name.capitalize() + "; " + self.name.capitalize() + " color: %s " % self.color.capitalize()
+
+class RedMeat(Food):
+    type = "meat"
+    def __str__(self):
+        #print("Red Meat: %s " % self.name.capitalize())
+        return "Red Meat: %s " % self.name.capitalize() + "; " + self.name.capitalize() + " color: %s " % self.color.capitalize()
+
+dictionary = [Food("Bread", "Brown"), RedMeat("Steak", "Dark Brown"), Poultry("Chicken Thighs", "Charred"), RootVeg("Potatoes", "Red"), Green("Broccoli", "Green")]
+print(dictionary)
