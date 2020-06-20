0d195bdc37897352037db0b60d8ce64d3924377b
Tim Shields <tim.shields132@gmail.com>
2020-06-20 19:49:09 -0400

    most recent

--- test.py
+++ test.py
@@ -11,26 +11,34 @@ class Food:
 class Green(Food):
     type = "veggie"
     def __str__(self):
-        #print("Green vegatable: %s " % self.name.capitalize())
         return "Green vegatable: %s " % self.name.capitalize() + "; " + self.name.capitalize() + " color: %s " % self.color.capitalize()
 
 class RootVeg(Food):
     type = "veggie"
     def __str__(self):
-        #print("Root vegatable: %s " % self.name.capitalize())
         return "Root vegatable: %s " % self.name.capitalize() + "; " + self.name.capitalize() + " color: %s " % self.color.capitalize()
         
 class Poultry(Food):
     type = "meat"
     def __str__(self):
-        #print("Poultry: %s " % self.name.capitalize())
         return "Poultry: %s " % self.name.capitalize() + "; " + self.name.capitalize() + " color: %s " % self.color.capitalize()
 
 class RedMeat(Food):
     type = "meat"
     def __str__(self):
-        #print("Red Meat: %s " % self.name.capitalize())
         return "Red Meat: %s " % self.name.capitalize() + "; " + self.name.capitalize() + " color: %s " % self.color.capitalize()
 
 dictionary = [Food("Bread", "Brown"), RedMeat("Steak", "Dark Brown"), Poultry("Chicken Thighs", "Charred"), RootVeg("Potatoes", "Red"), Green("Broccoli", "Green")]
-print(dictionary)
+fooddictionary = {food.name: food for food in dictionary}
+print(fooddictionary)
+
+def judgeMeal(plate):
+    dictionary = [fooddictionary[dictionary] for food in plate]
+    veg = sum(each.type == "veggie" or each.type == "unknown" for each in dictionary)
+    meat = sum(each.type == "meat" for each in dictionary)
+    judgement = "good"
+    if (meat > veg):
+        judgement = "bad"
+    return judgement
+
+plate = ["Chicken Thighs",]
