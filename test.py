6c88368b6c26f09ce3d8ff27163f3eb75326b37b
Tim Shields <tim.shields132@gmail.com>
2020-06-21 01:22:52 -0400

    fixed judge

--- test.py
+++ test.py
@@ -28,17 +28,19 @@ class RedMeat(Food):
     def __str__(self):
         return "Red Meat: %s " % self.name.capitalize() + "; " + self.name.capitalize() + " color: %s " % self.color.capitalize()
 
-dictionary = [Food("Bread", "Brown"), RedMeat("Steak", "Dark Brown"), Poultry("Chicken Thighs", "Charred"), RootVeg("Potatoes", "Red"), Green("Broccoli", "Green")]
-fooddictionary = {food.name: food for food in dictionary}
+foods = [Food("Bread", "Brown"), RedMeat("Steak", "Dark Brown"), Poultry("Chicken Thighs", "Charred"), RootVeg("Potatoes", "Red"), Green("Broccoli", "Green")]
+fooddictionary = {food.name: food for food in foods}
 print(fooddictionary)
 
 def judgeMeal(plate):
-    dictionary = [fooddictionary[dictionary] for food in plate]
-    veg = sum(each.type == "veggie" or each.type == "unknown" for each in dictionary)
-    meat = sum(each.type == "meat" for each in dictionary)
+    foods = [fooddictionary[food] for food in plate]
+    veg = sum(each.type == "veggie" or each.type == "unknown" for each in foods)
+    meat = sum(each.type == "meat" for each in foods)
     judgement = "good"
     if (meat > veg):
         judgement = "bad"
     return judgement
 
-plate = ["Chicken Thighs",]
+plate = ["Chicken Thighs", "Steak", "Potatoes", "Broccoli", "Steak"]
+judgement = judgeMeal(plate)
+print(judgement)
