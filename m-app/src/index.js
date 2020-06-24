d9e081f0faa763dcbde4ba7dc3558ac34e3b6d28
Tim Shields <tim.shields132@gmail.com>
2020-06-24 15:38:56 -0400

    work in index js

--- m-app/src/index.js
+++ m-app/src/index.js
@@ -2,25 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state ={
-            value: null,
-        };
-    }
-    //In JavaScript classes, you need to always call super when defining the constructor of a subclass. All React component classes that have a constructor should start with a super(props) call.
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => this.setState({value: 'X'})}
-        >
-          {this.state.value}
+function Square(props) {
+    return(
+        <button className="square" onClick={props.onClick}>
+            {props.value}
         </button>
-      );
-    }
-  }
+    );
+}
   
   class Board extends React.Component {
     constructor(props) {
@@ -30,8 +18,19 @@ class Square extends React.Component {
         };
     }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-      return <Square value={(i)} />;
+      return (
+        <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
