47ac95361be0baf50ea4016c0250606fe352d9ea
Tim Shields <tim.shields132@gmail.com>
2020-06-27 21:10:07 -0400

    done

---
+++ my-poosp/src/components/square.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './game';
+import './board';
+
+export function Square(props: any) {
+    return(
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+//When we modified the Square to be a function component, we also changed onClick={() => this.props.onClick()} to a shorter onClick={props.onClick} (note the lack of parentheses on both sides).
+  
+
