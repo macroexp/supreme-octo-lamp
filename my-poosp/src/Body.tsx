4bc6fbfb7cde49d55e137348035ee2fa2cab8037
Tim Shields <tim.shields132@gmail.com>
2020-06-29 15:19:45 -0400

    unbroke everything, working text body

--- my-poosp/src/Body.tsx
+++ my-poosp/src/Body.tsx
@@ -3,8 +3,8 @@ import { Typography, Divider } from 'antd';
 
 const { Title, Paragraph, Text } = Typography;
 
-const text: FC = () => (
-    <div className="text">
+const Body: FC = () => (
+    <div className="Body">
         <Typography>
             <Title>
                 Introduction
@@ -12,10 +12,16 @@ const text: FC = () => (
             <Paragraph>
                 This is a paragraph. The COVID-19 pandemic, also known as the coronavirus pandemic, is an ong
                 oing global pandemic of coronavirus disease 2019 (COVID‑19), caused by severe acute respirato
-                ry syndrome coronavirus 2 (SARS‑CoV‑2).[1] The outbreak was first identified in Wuhan, China,
-                 in December 2019.[4][6] The World Health Organization declared the outbreak a Public Health E
-                 mergency of International Concern on 30 Ja
+                ry syndrome coronavirus 2 (SARS‑CoV‑2).[1] 
+                <Text strong>
+                    The outbreak was first identified in Wuhan, China,in December 2019.[4][6] The World Health Or
+                    ganization declared the outbreak a Public Health Emergency of International Concern on 30 Ja
+                </Text>
+                <br></br><br></br>
+                Press <Text keyboard>Esc</Text> to exit...
             </Paragraph>
         </Typography>
     </div>
-)
+);
+
+export default Body
--- my-poosp/src/index.tsx
+++ my-poosp/src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Body from './Body';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <text></text>
+    <Body></Body>
   </React.StrictMode>,
   document.getElementById('root')
 );
