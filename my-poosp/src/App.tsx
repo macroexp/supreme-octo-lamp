c21f840cbbb0f680ddfbef239069b332c526f026
Tim Shields <tim.shields132@gmail.com>
2020-06-28 23:41:55 -0400

    adding antd

--- my-poosp/src/App.tsx
+++ my-poosp/src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Game } from './components/game';
+import { Button } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <Game></Game>
-    </div>
-  );
-}
+//function App() {
+  //return (
+    //<div className="App">
+      //<Game></Game>
+    //</div>
+  //);
+//}
+
+//export default App;
+
+const App: FC = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+    <Game></Game>
+  </div>
+);
 
 export default App;
