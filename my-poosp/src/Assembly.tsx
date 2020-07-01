f663f83eedfa9807189f8a828a45c892c9b8c54c
Tim Shields <tim.shields132@gmail.com>
2020-06-30 21:16:09 -0400

    stuff

--- my-poosp/src/Assembly.tsx
+++ my-poosp/src/Assembly.tsx
@@ -1,11 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import './Assembly.css'
 import { Typography, Divider, Space, Button, Tooltip, Layout, Menu, Breadcrumb } from 'antd';
 import { SearchOutlined, AppstoreOutlined,BarChartOutlined,CloudOutlined,
     ShopOutlined, TeamOutlined, UserOutlined, UploadOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import App from './App';
+import { SelectParam } from 'antd/lib/menu';
+import { Game } from './components/game';
 
 const { Title, Paragraph, Text, Link } = Typography;
-const { Header, Content, Footer, Sider } = Layout
+const { Header, Content, Footer, Sider } = Layout;
 
 class Demo extends React.Component {
     state = {
@@ -29,89 +32,100 @@ class Demo extends React.Component {
     }
 }
 
-const Format: FC = () => (
-    <Layout>
-        <Header style={{position:'fixed', zIndex: 1, width:'100%'}}>
-            <div className = "Logo"></div>
-            <Menu theme="dark" mode="horizontal" style={{paddingLeft:150}} defaultSelectedKeys={['2']}>
-                <Menu.Item key="1">About 1</Menu.Item>
-                <Menu.Item key="2">About 2</Menu.Item>
-                <Menu.Item key="3">About 3</Menu.Item>
-            </Menu>
-        </Header>
-        <Sider style={{
-            overflow: 'auto',
-            height: '100vh',
-            position: 'fixed',
-            left: 0,
-        }}>
-            <Menu theme="dark" mode="inline" style={{paddingTop: 80}} defaultSelectedKeys={['3']}>
-                <Menu.Item key="1" icon={<UserOutlined />}>Option 1</Menu.Item>
-                <Menu.Item key="2" icon={<VideoCameraOutlined />}>Option 2</Menu.Item>
-                <Menu.Item key="3" icon={<UploadOutlined />}>Option 3</Menu.Item>
-                <Menu.Item key="4" icon={<TeamOutlined />}>Option 4</Menu.Item>
-            </Menu>
-        </Sider>
-        <Layout className="site-layout" style={{marginLeft: 200}}>
-            <Header className="site-layout-background" style={{padding: 0}}></Header>
-            <Content style={{margin:'24px 16px 0', overflow: 'initial'}}>
-                <Typography className="site-layout-text" style={{padding: 24, textAlign:'left'}}>
-                    <Button type="primary" block size="large">Big Button</Button>
-                    <Button type="default" danger>Default Button</Button>
-                    <Button type="dashed" disabled>
-                        DisabledDashed
-                    </Button>
 
-                    <Tooltip title="search">
-                        <Button type="primary" shape="round" icon={<SearchOutlined />}></Button>
-                    </Tooltip>
 
-                    <Title>Default Title.</Title>
-                    <Title level={2}>Level 2 Title. Goes down to 4.</Title>
-                    <Title>
-                        <br></br>
-                        
-                        Introduction
-                    </Title>
-                    <Paragraph>
-                        This is a paragraph. The COVID-19 pandemic, also known as the coronavirus pandemic, is an ong
-                        oing global pandemic of coronavirus disease 2019 (COVID‑19), caused by severe acute respirato
-                        ry syndrome coronavirus 2 (SARS‑CoV‑2).[1] 
-                        <Text strong>
-                            The outbreak was first identified in Wuhan, China,in December 2019.[4][6] The World Health Or
-                            ganization declared the outbreak a Public Health Emergency of International Concern on 30 Ja
-                        </Text>
-                    </Paragraph>
-                    <Space direction="vertical">
-                        <Text>Text</Text>
-                        <Text type="warning">Warning Text</Text>
-                        <Text strong underline>Strong Underlined Text</Text>
-                        <Link href="https://ant.design/components/typography/">
-                            For all text varieties click here.
-                        </Link>
-                    </Space>
-                    <Demo />
-                    <Breadcrumb style={{margin:'16px 0', padding:'0 50px'}}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item>
-                            <a href="">Thing</a>
-                        </Breadcrumb.Item>
-                        <Breadcrumb.Item>App</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div className="site-layout-content">
+const Format: FC = () => {
+    const [CurrentTab, SetCurrentTab] = useState("1");
+    const SelectTab = (param: SelectParam) => {
+        SetCurrentTab(param.key)
+    }
+    if(CurrentTab == "1")
+        return <Layout>
+            <Header style={{position:'fixed', zIndex: 1, width:'100%'}}>
+                <div className = "Logo"></div>
+                <Menu theme="dark" mode="horizontal" style={{paddingLeft:150}} defaultSelectedKeys={['3']}>
+                    <Menu.Item key="1">About 1</Menu.Item>
+                    <Menu.Item key="2">About 2</Menu.Item>
+                    <Menu.Item key="3">About 3</Menu.Item>
+                </Menu>
+            </Header>
+            <Sider style={{
+                overflow: 'auto',
+                height: '100vh',
+                position: 'fixed',
+                left: 0,
+            }}>
+                <Menu onSelect={SelectTab} theme="dark" mode="inline" style={{paddingTop: 80}} defaultSelectedKeys={['3']}>
+                    <Menu.Item key="1" icon={<UserOutlined />}>Option 1</Menu.Item>
+                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>Option 2</Menu.Item>
+                    <Menu.Item key="3" icon={<UploadOutlined />}>Option 3</Menu.Item>
+                    <Menu.Item key="4" icon={<TeamOutlined />}>Option 4</Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout className="site-layout" style={{marginLeft: 200}}>
+                <Header className="site-layout-background" style={{padding: 0}}></Header>
+                <Content style={{margin:'24px 16px 0', overflow: 'initial'}}>
+                    <Typography className="site-layout-text" style={{padding: 24, textAlign:'left'}}>
+                        <Button type="primary" block size="large">Big Button</Button>
+                        <Button type="default" danger>Default Button</Button>
+                        <Button type="dashed" disabled>
+                            DisabledDashed
+                        </Button>
 
-                    <br></br><br></br>
+                        <Tooltip title="search">
+                            <Button type="primary" shape="round" icon={<SearchOutlined />}></Button>
+                        </Tooltip>
 
-                    </div>
-                    <Footer style={{margin:'24px -50px 0'}}>
-                        Antd copyright 2020
-                        Press <Text keyboard>Poop?</Text> to exit...
-                    </Footer>
-                </Typography>
-            </Content>
+                        <Title>Default Title.</Title>
+                        <Title level={2}>Level 2 Title. Goes down to 4.</Title>
+                        <Title>
+                            <br></br>
+                            
+                            Introduction
+                        </Title>
+                        <Paragraph>
+                            This is a paragraph. The COVID-19 pandemic, also known as the coronavirus pandemic, is an ong
+                            oing global pandemic of coronavirus disease 2019 (COVID‑19), caused by severe acute respirato
+                            ry syndrome coronavirus 2 (SARS‑CoV‑2).[1] 
+                            <Text strong>
+                                The outbreak was first identified in Wuhan, China,in December 2019.[4][6] The World Health Or
+                                ganization declared the outbreak a Public Health Emergency of International Concern on 30 Ja
+                            </Text>
+                        </Paragraph>
+                        <Space direction="vertical">
+                            <Text>Text</Text>
+                            <Text type="warning">Warning Text</Text>
+                            <Text strong underline>Strong Underlined Text</Text>
+                            <Link href="https://ant.design/components/typography/">
+                                For all text varieties click here.
+                            </Link>
+                        </Space>
+                        <Demo />
+                        <Breadcrumb style={{margin:'16px 0', padding:'0 50px'}}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <a href="">Thing</a>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>App</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div className="site-layout-content">
+
+                        <br></br><br></br>
+
+                        </div>
+                        <Footer style={{margin:'24px -50px 0'}}>
+                            Antd copyright 2020
+                            Press <Text keyboard>Poop?</Text> to exit...
+                        </Footer>
+                    </Typography>
+                </Content>
+            </Layout>
         </Layout>
-    </Layout>
-);
+    else if(CurrentTab == "2")
+        return <Game />
+    else
+        return <div />
+};
 
 export default Format;
 
--- my-poosp/src/index.tsx
+++ my-poosp/src/index.tsx
@@ -8,7 +8,6 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Format></Format>
-    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
