9280674f0cb8fc3cced472cba5c7f9f7129a55e9
Tim Shields <tim.shields132@gmail.com>
2020-07-11 16:19:45 -0400

    Setting up index.ts and webpack

--- express-ts-api/src/index.ts
+++ express-ts-api/src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { hostname } from "os";
 
 dotenv.config();
 
@@ -40,3 +41,25 @@ const server = app.listen(PORT, () => {
 /**
  * Webpack HMR Activation
  */
+
+type ModuleId = string | number;
+
+interface WebpackHotModule {
+    hot?: {
+        data: any;
+        accept(
+            dependencies: string[],
+            callback?: (updatedDependencies: ModuleId[]) => void,
+        ): void;
+        accept(dependency: string, callback?: () => void): void;
+        accept(errHandler?: (err: Error) => void): void;
+        dispose(callback: (data: any) => void): void;
+    };
+}
+
+declare const module: WebpackHotModule;
+
+if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => server.close());
+}
