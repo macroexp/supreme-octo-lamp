03bc29a2ad88480cb3e9b38eb765d86b9cae7b0d
Tim Shields <tim.shields132@gmail.com>
2020-07-11 17:02:06 -0400

    new api

--- express-ts-api/dist/index.js
+++ express-ts-api/dist/index.js
@@ -22,7 +22,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "29f715c89e15eef77605";
+/******/ 	var hotCurrentHash = "9ef03ce70fee71f2eb9c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -826,7 +826,7 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\n/**\n * Required External Modules\n */\nvar __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {\n    if (k2 === undefined) k2 = k;\n    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });\n}) : (function(o, m, k, k2) {\n    if (k2 === undefined) k2 = k;\n    o[k2] = m[k];\n}));\nvar __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {\n    Object.defineProperty(o, \"default\", { enumerable: true, value: v });\n}) : function(o, v) {\n    o[\"default\"] = v;\n});\nvar __importStar = (this && this.__importStar) || function (mod) {\n    if (mod && mod.__esModule) return mod;\n    var result = {};\n    if (mod != null) for (var k in mod) if (k !== \"default\" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);\n    __setModuleDefault(result, mod);\n    return result;\n};\nvar __importDefault = (this && this.__importDefault) || function (mod) {\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\n};\nObject.defineProperty(exports, \"__esModule\", { value: true });\nvar dotenv = __importStar(__webpack_require__(/*! dotenv */ \"dotenv\"));\nvar express_1 = __importDefault(__webpack_require__(/*! express */ \"express\"));\nvar cors_1 = __importDefault(__webpack_require__(/*! cors */ \"cors\"));\nvar helmet_1 = __importDefault(__webpack_require__(/*! helmet */ \"helmet\"));\ndotenv.config();\n/**\n * App Variables\n */\nif (!process.env.PORT) {\n    process.exit(1);\n}\nvar PORT = parseInt(process.env.PORT, 10);\nvar app = express_1.default();\n/**\n *  App Configuration\n */\napp.use(helmet_1.default());\napp.use(cors_1.default());\napp.use(express_1.default.json());\n/**\n * Server Activation\n */\nvar server = app.listen(PORT, function () {\n    console.log('Listening on port ${PORT}');\n});\nif (true) {\n    module.hot.accept();\n    module.hot.dispose(function () { return server.close(); });\n}\n\n\n//# sourceURL=webpack:///./src/index.ts?");
+eval("\n/**\n * Required External Modules\n */\nvar __importDefault = (this && this.__importDefault) || function (mod) {\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\n};\nObject.defineProperty(exports, \"__esModule\", { value: true });\nvar express_1 = __importDefault(__webpack_require__(/*! express */ \"express\"));\nvar cors_1 = __importDefault(__webpack_require__(/*! cors */ \"cors\"));\nvar helmet_1 = __importDefault(__webpack_require__(/*! helmet */ \"helmet\"));\nvar dotenv_1 = __webpack_require__(/*! dotenv */ \"dotenv\");\ndotenv_1.config();\n/**\n * App Variables\n */\nif (!process.env.PORT) {\n    process.exit(1);\n}\nvar PORT = parseInt(process.env.PORT, 10);\nvar app = express_1.default();\n/**\n *  App Configuration\n */\napp.use(helmet_1.default());\napp.use(cors_1.default());\napp.use(express_1.default.json());\n/**\n * Server Activation\n */\nvar server = app.listen(PORT, function () {\n    console.log('Listening on port ${PORT}');\n});\n\n\n//# sourceURL=webpack:///./src/index.ts?");
 
 /***/ }),
 
--- express-ts-api/src/index.ts
+++ express-ts-api/src/index.ts
@@ -2,13 +2,13 @@
  * Required External Modules
  */
 
-import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { hostname } from "os";
+import {config} from "dotenv";
 
-dotenv.config();
+config();
 
 /**
  * App Variables
@@ -38,28 +38,3 @@ const server = app.listen(PORT, () => {
     console.log('Listening on port ${PORT}');
 });
 
-/**
- * Webpack HMR Activation
- */
-
-type ModuleId = string | number;
-
-interface WebpackHotModule {
-    hot?: {
-        data: any;
-        accept(
-            dependencies: string[],
-            callback?: (updatedDependencies: ModuleId[]) => void,
-        ): void;
-        accept(dependency: string, callback?: () => void): void;
-        accept(errHandler?: (err: Error) => void): void;
-        dispose(callback: (data: any) => void): void;
-    };
-}
-
-declare const module: WebpackHotModule;
-
-if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => server.close());
-}
